e8449085a8d81e3d0d9003a97d09c16d4a5e8f56
Zach Kauffman <zach.kauffman0@gmail.com>
2025-04-25 01:39:28 -0700

    can tab between folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react';
 import './App.css'
+import { Folder } from './folder/Folder';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [selected, setSelected] = useState<number>(0);
+
+  const handleClick = (index: number) => {
+    console.log('selected', index);
+    if (index !== tabs.length) setSelected(index);
+  }
+
+  const tabs: string[] = [
+    'content 1',
+    'content 2',
+    'content 3',
+    'content 4',
+    'content 5',
+    'content 6',
+  ];
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{
+    }}>
+      {tabs.map((content, index) => index !== selected && (
+        <Folder label={content} index={index} onClick={handleClick}><>poopy</></Folder>
+      ))}
+      <Folder label={tabs[selected]} index={tabs.length} onClick={handleClick}><>poopy</></Folder>
+    </div>
   )
 }
 
