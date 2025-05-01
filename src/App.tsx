4fb708a0334802b928974f4896d9b3bb184fd40a
Zach Kauffman <zach.kauffman0@gmail.com>
2025-04-30 21:01:33 -0700

    Setup eslint and prettier, format project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useCallback, useState } from 'react';
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { Folder } from './folder/Folder';
 
-
 function App() {
-
   const [selected, setSelected] = useState<number>(0);
 
   const handleClick = (index: number) => {
     console.log('selected', index);
     if (index !== tabs.length) setSelected(index);
-  }
+  };
 
   const tabs: [string, string][] = [
     ['Resume', 'resume text here'],
@@ -26,20 +24,25 @@ function App() {
     return tabs.map((content, index) => {
       if (index !== selected) {
         i += 1;
-        return <Folder label={content[0]} location={i} index={index} onClick={handleClick} />
+        return <Folder label={content[0]} location={i} index={index} onClick={handleClick} />;
       }
-    }
-    )
-  }
-
+    });
+  };
 
   return (
-    <div style={{
-    }}>
+    <div style={{}}>
       {unselectedFolders()}
-      <Folder label={tabs[selected][0]} location={selected} index={tabs.length} onClick={handleClick} isSelected><>{tabs[selected][1]}</></Folder>
+      <Folder
+        label={tabs[selected][0]}
+        location={selected}
+        index={tabs.length}
+        onClick={handleClick}
+        isSelected
+      >
+        <>{tabs[selected][1]}</>
+      </Folder>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/folder/Folder.tsx
+++ src/folder/Folder.tsx
@@ -1,135 +1,139 @@
-import React from "react";
-import { BORDER_COLOR, MANILLA } from "../theme";
+import React from 'react';
+import { BORDER_COLOR, MANILLA } from '../theme';
 
 interface FolderProps {
-    label: string;
-    children?: React.ReactElement;
-    index: number;
-    location: number;
-    onClick: (index: number) => void;
-    isSelected?: boolean;
+  label: string;
+  children?: React.ReactElement;
+  index: number;
+  location: number;
+  onClick: (index: number) => void;
+  isSelected?: boolean;
 }
 
 // Folder.tsx
 export const Folder = ({
-    label,
-    children,
-    index,
-    location,
-    onClick,
-    isSelected = false,
+  label,
+  children,
+  index,
+  location,
+  onClick,
+  isSelected = false,
 }: FolderProps) => {
-    const tab = () => (
-        <div
-            onClick={() => onClick(index)}
-            style={{
-                position: "absolute",
-                top: "-1.58rem",
-                left: isSelected ? 25 : `${((location % 5 * 50) + 10) * 2.5}px`,
-                display: "flex",
-                alignItems: "center",
-                zIndex: location + 1,
-                cursor: isSelected ? undefined : 'pointer',
-            }}
+  const tab = () => (
+    <div
+      onClick={() => onClick(index)}
+      style={{
+        position: 'absolute',
+        top: '-1.58rem',
+        left: isSelected ? 25 : `${((location % 5) * 50 + 10) * 2.5}px`,
+        display: 'flex',
+        alignItems: 'center',
+        zIndex: location + 1,
+        cursor: isSelected ? undefined : 'pointer',
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          left: '-9px',
+          width: '10px',
+          height: '25.5px',
+          backgroundColor: BORDER_COLOR,
+          transform: 'skewX(-20deg)',
+          borderTopLeftRadius: '4px',
+          zIndex: 2,
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          left: '-7px',
+          bottom: '0px',
+          width: '20px',
+          height: '20.5px',
+          backgroundColor: MANILLA,
+          transform: 'skewX(-20deg)',
+          borderTopLeftRadius: '4px',
+          zIndex: 2,
+        }}
+      />
+      <div
+        style={{
+          height: '5.5px',
+          padding: '0.5rem 1rem',
+          backgroundColor: MANILLA,
+          fontWeight: 'bold',
+          zIndex: 3,
+          borderTop: `4px solid ${BORDER_COLOR}`,
+          borderTopLeftRadius: '6px',
+          borderTopRightRadius: '6px',
+        }}
+      >
+        <p
+          style={{
+            margin: '-6px 0 0',
+          }}
         >
-            <div
-                style={{
-                    position: 'absolute',
-                    left: '-9px',
-                    width: "10px",
-                    height: "25.5px",
-                    backgroundColor: BORDER_COLOR,
-                    transform: "skewX(-20deg)",
-                    borderTopLeftRadius: "4px",
-                    zIndex: 2,
-                }}
-            />
-            <div
-                style={{
-                    position: 'absolute',
-                    left: '-7px',
-                    bottom: '0px',
-                    width: "20px",
-                    height: "20.5px",
-                    backgroundColor: MANILLA,
-                    transform: "skewX(-20deg)",
-                    borderTopLeftRadius: "4px",
-                    zIndex: 2,
-                }}
-            />
-            <div
-                style={{
-                    height: '5.5px',
-                    padding: "0.5rem 1rem",
-                    backgroundColor: MANILLA,
-                    fontWeight: "bold",
-                    zIndex: 3,
-                    borderTop: `4px solid ${BORDER_COLOR}`,
-                    borderTopLeftRadius: "6px",
-                    borderTopRightRadius: "6px",
-                }}
-            >
-            <p style={{
-                margin: '-6px 0 0',
-            }}>{label}</p>
-            </div>
-            <div
-                style={{
-                    position: 'absolute',
-                    right: '-9px',
-                    width: "10px",
-                    height: "25.5px",
-                    backgroundColor: BORDER_COLOR,
-                    transform: "skewX(20deg)",
-                    borderTopRightRadius: "4px",
-                    zIndex: 2,
-                }}
-            />
-            <div
-                style={{
-                    position: 'absolute',
-                    right: '-7px',
-                    bottom: '0px',
-                    width: "20px",
-                    height: "20.5px",
-                    backgroundColor: MANILLA,
-                    transform: "skewX(20deg)",
-                    borderTopRightRadius: "4px",
-                    zIndex: 2,
-                }}
-            />
-        </div>);
+          {label}
+        </p>
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          right: '-9px',
+          width: '10px',
+          height: '25.5px',
+          backgroundColor: BORDER_COLOR,
+          transform: 'skewX(20deg)',
+          borderTopRightRadius: '4px',
+          zIndex: 2,
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          right: '-7px',
+          bottom: '0px',
+          width: '20px',
+          height: '20.5px',
+          backgroundColor: MANILLA,
+          transform: 'skewX(20deg)',
+          borderTopRightRadius: '4px',
+          zIndex: 2,
+        }}
+      />
+    </div>
+  );
 
-    return (
-        <div
-            style={{
-                position: "absolute",
-                top: index * 10 + 30,
-                left: index * 12,
-                right: 0,
-                margin: "auto",
-                width: "80%",
-                height: "80%",
-                backgroundColor: MANILLA,
-                borderLeft: `4px solid ${BORDER_COLOR}`,
-                borderRight: `4px solid ${BORDER_COLOR}`,
-                borderRadius: "8px",
-                boxShadow: "0 4px 10px rgba(0,0,0,0.2)",
-                zIndex: index,
-                overflow: "visible",
-            }}
-        >
-            {tab()}
-            <div
-                style={{
-                    padding: "2rem",
-                    paddingTop: "3rem",
-                    borderTop: `4px solid ${BORDER_COLOR}`,
-                }}
-            >
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: index * 10 + 30,
+        left: index * 12,
+        right: 0,
+        margin: 'auto',
+        width: '80%',
+        height: '80%',
+        backgroundColor: MANILLA,
+        borderLeft: `4px solid ${BORDER_COLOR}`,
+        borderRight: `4px solid ${BORDER_COLOR}`,
+        borderRadius: '8px',
+        boxShadow: '0 4px 10px rgba(0,0,0,0.2)',
+        zIndex: index,
+        overflow: 'visible',
+      }}
+    >
+      {tab()}
+      <div
+        style={{
+          padding: '2rem',
+          paddingTop: '3rem',
+          borderTop: `4px solid ${BORDER_COLOR}`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
-
